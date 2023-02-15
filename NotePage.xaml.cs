1f423bc470d3d50d7fc5dd41f1969e5ba13dc8c8
Anderson Vieira <andersonvieira@MacBook-Pro-de-Anderson.local>
2023-02-15 08:29:43 -0300

    Projeto de notas

---
+++ NotePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace Notes;
+
+public partial class NotePage : ContentPage
+{
+	string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+	public NotePage()
+	{
+		InitializeComponent();
+
+		if (File.Exists(_fileName))
+			TextEditor.Text = File.ReadAllText(_fileName);
+	}
+
+    void SaveButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+		// Save the file
+		File.WriteAllText(_fileName, TextEditor.Text);
+    }
+
+    void DeleteButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+		// Delete the file
+		if (File.Exists(_fileName))
+			File.Delete(_fileName);
+
+		TextEditor.Text = string.Empty;
+    }
+}
